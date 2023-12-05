749944a2d21e5942a3323a7a723fa8e5cf679cc8
Douglas <douglassilvaalves336@gmail.com>
2023-12-05 20:12:08 -0300

    PrimeiroCommit

---
+++ src/projetotrabalho01/ProjetoTrabalho01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetotrabalho01;
+
+/**
+ *
+ * @author 182220031
+ */
+public class ProjetoTrabalho01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
